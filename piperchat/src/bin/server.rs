84973508336d91e0b2ed2985628d71da36eb0d85
Marcel Guzik <mguzik555@gmail.com>
2022-12-30 18:05:46 +0100

    Working Rust signalling server

--- piperchat/src/bin/server.rs
+++ piperchat/src/bin/server.rs
@@ -1,108 +1,172 @@
-use std::sync::{Arc, Mutex};
-
-use futures_util::{SinkExt, StreamExt};
+use anyhow::{anyhow, bail};
+use futures::{SinkExt, StreamExt};
+use log::info;
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
 use tokio::{
     net::{TcpListener, TcpStream},
     select,
     sync::mpsc,
 };
-use tokio_tungstenite::tungstenite::Message;
+type WsMessage = tokio_tungstenite::tungstenite::Message;
+
+#[derive(Debug)]
+enum Command {
+    SendMessage(String),
+    SessionJoin(mpsc::UnboundedSender<Command>),
+}
+
+struct State {
+    users: HashMap<usize, User>,
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            users: HashMap::new(),
+        }
+    }
+}
 
 #[derive(Debug)]
 struct User {
     name: String,
-    tx: mpsc::UnboundedSender<String>,
+    id: usize,
+    tx: mpsc::UnboundedSender<Command>,
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
+    pretty_env_logger::init();
     let listener = TcpListener::bind("0.0.0.0:2137").await.unwrap();
-    let connected_users = Arc::new(Mutex::new(Vec::<User>::new()));
+    let state = Arc::new(Mutex::new(State::new()));
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
-        let connected_users = connected_users.clone();
+        let state = state.clone();
         tokio::spawn(async move {
-            process(socket, connected_users).await;
+            process(socket, state).await.unwrap();
         });
     }
 }
 
-async fn process(socket: TcpStream, users: Arc<Mutex<Vec<User>>>) {
-    let ws = tokio_tungstenite::accept_async(socket).await.unwrap();
+async fn process(socket: TcpStream, state: Arc<Mutex<State>>) -> anyhow::Result<()> {
+    let ws = tokio_tungstenite::accept_async(socket).await?;
     let (mut ws_sink, mut ws_stream) = ws.split();
 
     // receive user name as bytes into socket
-    let name = match ws_stream.next().await {
-        Some(Ok(Message::Text(name))) => name,
+    let message = match ws_stream.next().await {
+        Some(Ok(WsMessage::Text(payload))) => payload,
         _ => {
-            eprintln!("user didn't provide a name");
-            return;
+            bail!("bad message");
         }
     };
-    let name = name.trim();
-    println!("{name} connected.");
+
+    let (command, id) = message.split_once(" ").unwrap();
+    if command != "HELLO" {
+        bail!("Connect message invalid")
+    }
+
+    let id: usize = id.parse()?;
+    ws_sink.send(WsMessage::text("HELLO")).await?;
+    info!("got HELLO");
+
+    println!("{id} connected.");
 
     // construct user
     let (tx, mut rx) = mpsc::unbounded_channel();
     let user = User {
-        name: name.to_owned(),
+        name: "john".to_owned(),
+        id,
         tx: tx.clone(),
     };
 
     {
-        let mut users = users.lock().unwrap();
-        users.push(user);
+        let mut state = state.lock().unwrap();
+        let users = &mut state.users;
+        users.insert(user.id, user);
+        // broadcast_user_list(&users);
     }
 
-    broadcast_user_list(users.clone()).await;
+    let mut session_peer: Option<mpsc::UnboundedSender<Command>> = None;
 
     loop {
         select! {
-            message = rx.recv() => {
-                match message {
-                    Some(message) => {
+            command = rx.recv() => {
+                info!("sending: {message:?}");
+                match command {
+                    Some(Command::SendMessage(message)) => {
                         ws_sink.send(message.into()).await.unwrap();
+                    }
+                    Some(Command::SessionJoin(peer_sink)) => {
+                        session_peer = Some(peer_sink);
                     }
                     None => break,
                 }
             },
 
+            // message from user socket
+            // if not in session:
+            // - SESSION_OK to create session
+            // if in session:
+            //   just reroute everything to session peer
             message = ws_stream.next() => {
                 match message {
-                    Some(Ok(message)) => (),
+                    Some(Ok(WsMessage::Text(message))) => {
+                        info!("received: {message:?}");
+
+                        if let Some(peer) = session_peer.as_ref() {
+                            peer.send(Command::SendMessage(message.clone()))?;
+                        }
+
+                        if session_peer.is_none() {
+                            let (command, id) = message.split_once(" ").unwrap();
+                            if command == "SESSION" {
+                                {
+                                    let state = state.lock().unwrap();
+                                    let session_id = id.parse()?;
+                                    let peer_tx = &state.users.get(&session_id).ok_or(anyhow!("no such session"))?.tx;
+                                    peer_tx.send(Command::SessionJoin(tx.clone()))?;
+                                    session_peer = Some(peer_tx.clone());
+
+                                }
+                                ws_sink.send(WsMessage::text("SESSION_OK")).await?;
+                            }
+                        }
+                    },
                     _ => break
                 }
             }
         }
     }
 
-    println!("{name} disconnected.");
+    println!("{id} disconnected.");
 
     {
-        let mut users = users.lock().unwrap();
-        let idx = users.iter().position(|u| u.name == name).unwrap();
-        users.remove(idx);
+        let mut state = state.lock().unwrap();
+        let users = &mut state.users;
+        users.remove(&id);
+        // broadcast_user_list(&users);
     }
 
-    broadcast_user_list(users.clone()).await;
+    Ok(())
 }
 
-async fn broadcast_user_list(users: Arc<Mutex<Vec<User>>>) {
+fn broadcast_user_list(users: &HashMap<usize, User>) {
     // construct message with all connected users
     let message = {
-        let users = users.lock().unwrap();
         let user_names: Vec<_> = users
-            .iter()
-            .map(|user| format!("- {}", user.name))
+            .values()
+            .map(|user| format!("- {}: {}", user.name, user.id))
             .collect();
         let user_names = user_names.join("\n");
-        format!("CONNECTED USERS:\n{user_names}")
+        format!("sessions:\n{user_names}")
     };
 
     // send message to all users
-    let users = users.lock().unwrap();
-    for user in &*users {
-        user.tx.send(message.clone()).unwrap();
+    for user in users.values() {
+        user.tx.send(Command::SendMessage(message.clone())).unwrap();
     }
 }
--- piperchat/src/piperchat.rs
+++ piperchat/src/piperchat.rs
@@ -1,3 +1,28 @@
+use serde::{Deserialize, Serialize};
+
 pub mod window;
 
 pub const APP_ID: &str = "eu.mguzik.piperchat";
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Message {
+    Connect(String),
+    WebRtc(WebrtcMsg),
+}
+
+// JSON messages we communicate with
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum WebrtcMsg {
+    Ice {
+        candidate: String,
+        #[serde(rename = "sdpMLineIndex")]
+        sdp_mline_index: u32,
+    },
+    Sdp {
+        #[serde(rename = "type")]
+        type_: String,
+        sdp: String,
+    },
+}
