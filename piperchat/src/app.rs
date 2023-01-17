11be7173f3aad5c35e69ebe1f50df43f571b65c2
Marcel Guzik <mguzik555@gmail.com>
2023-01-17 17:22:19 +0100

    Change test audio pattern to mic

--- piperchat/src/app.rs
+++ piperchat/src/app.rs
@@ -84,7 +84,7 @@ impl App {
         // Create the GStreamer pipeline
         let pipeline = gst::parse_launch(
             "v4l2src ! videoconvert ! vp8enc deadline=1 ! rtpvp8pay pt=96 ! webrtcbin. \
-            audiotestsrc is-live=true ! opusenc ! rtpopuspay pt=97 ! webrtcbin. \
+            autoaudiosrc ! opusenc ! rtpopuspay pt=97 ! webrtcbin. \
             webrtcbin name=webrtcbin",
         )?;
 
